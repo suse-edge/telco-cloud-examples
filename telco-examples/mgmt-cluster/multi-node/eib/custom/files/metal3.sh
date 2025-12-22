87d2bfdfe3bd1efe214524e6669feac2cab9184e
Steven Hardy <steven.hardy@suse.com>
2025-12-22 14:44:17 +0000

    Align with rancher-turtles-providers chart
    This new chart replaces the previous rancher-turtles wrapper chart
    to align with the upstream turtles direction now that the turtles
    chart is integrated into Rancher 2.13
    
    We no longer need the airgap-resources chart because the new providers
    chart creates the airgap resources by default

--- telco-examples/mgmt-cluster/multi-node/eib/custom/files/metal3.sh
+++ telco-examples/mgmt-cluster/multi-node/eib/custom/files/metal3.sh
@@ -66,23 +66,6 @@ if [ $(${KUBECTL} get svc -n ${METAL3_CHART_TARGETNAMESPACE} metal3-metal3-ironi
 	EOF
 fi
 
-# If rancher is deployed
-if [ $(${KUBECTL} get pods -n ${RANCHER_CHART_TARGETNAMESPACE} -l app=rancher -o name | wc -l) -ge 1 ]; then
-  cat <<-EOF | ${KUBECTL} apply -f -
-	apiVersion: management.cattle.io/v3
-	kind: Feature
-	metadata:
-	  name: embedded-cluster-api
-	spec:
-	  value: false
-	EOF
-
-  # Disable Rancher webhooks for CAPI
-  ${KUBECTL} delete --ignore-not-found=true mutatingwebhookconfiguration.admissionregistration.k8s.io mutating-webhook-configuration
-  ${KUBECTL} delete --ignore-not-found=true validatingwebhookconfigurations.admissionregistration.k8s.io validating-webhook-configuration
-  ${KUBECTL} wait --for=delete namespace/cattle-provisioning-capi-system --timeout=300s
-fi
-
 # Clean up the lock cm
 
 ${KUBECTL} delete configmap ${METAL3LOCKCMNAME} -n ${METAL3LOCKNAMESPACE}
